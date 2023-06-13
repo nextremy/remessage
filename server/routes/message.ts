1b205bdf2e388a064cd3bd15e417f419fc45a1fd
Remy Wood <99709675+nextremy@users.noreply.github.com>
2023-06-12 18:42:35 -0700

    Add message read route

--- server/routes/message.ts
+++ server/routes/message.ts
@@ -3,6 +3,63 @@ import { CustomFastifyInstance } from "fastify";
 import prisma from "../prisma/client";
 
 export default async function messageRoutes(fastify: CustomFastifyInstance) {
+  fastify.get(
+    "/messages",
+    {
+      schema: {
+        querystring: Type.Object({
+          channelId: Type.String(),
+          limit: Type.Integer({
+            default: 50,
+            minimum: 1,
+            maximum: 100,
+          }),
+        }),
+        response: {
+          200: Type.Array(
+            Type.Object({
+              id: Type.String(),
+              timestamp: Type.Date(),
+              text: Type.String(),
+              senderId: Type.String(),
+              channelId: Type.String(),
+            })
+          ),
+        },
+      },
+    },
+    async (request) => {
+      const { id: sessionId } = request.session;
+      const { channelId, limit } = request.query;
+
+      const messages = await prisma.message.findMany({
+        select: {
+          id: true,
+          timestamp: true,
+          text: true,
+          senderId: true,
+          channelId: true,
+        },
+        where: {
+          channel: {
+            id: channelId,
+            participants: {
+              some: {
+                id: sessionId,
+              },
+            },
+          },
+        },
+        orderBy: {
+          timestamp: "asc",
+        },
+        take: limit,
+      });
+
+      return messages;
+    }
+  );
+
   fastify.post(
     "/send-message",
     {
