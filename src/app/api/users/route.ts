f84ee70c407eb77987008bc7ab5dffe6b8ca8149
Remy Wood <99709675+nextremy@users.noreply.github.com>
2023-06-22 14:19:36 -0700

    Implement user creation route handler

---
+++ src/app/api/users/route.ts
@@ -0,0 +1,41 @@
+import { Type } from "@sinclair/typebox";
+import { TypeCompiler } from "@sinclair/typebox/compiler";
+import { randomBytes, scryptSync } from "crypto";
+import { NextRequest, NextResponse } from "next/server";
+import { prisma } from "../../../prisma/client";
+
+const POSTBodyTypeCheck = TypeCompiler.Compile(
+  Type.Object({
+    username: Type.String({
+      minLength: 1,
+      maxLength: 16,
+      pattern: "^[a-z0-9_]*$",
+    }),
+    password: Type.String({
+      minLength: 8,
+      maxLength: 256,
+    }),
+  }),
+);
+
+export async function POST(request: NextRequest) {
+  const requestBody = await request.json();
+  if (!POSTBodyTypeCheck.Check(requestBody)) {
+    return NextResponse.json(null, { status: 400 });
+  }
+
+  const passwordSalt = randomBytes(32);
+  const passwordHash = scryptSync(requestBody.password, passwordSalt, 32);
+  const user = await prisma.user.create({
+    select: {
+      id: true,
+      username: true,
+    },
+    data: {
+      username: requestBody.username,
+      passwordHash,
+      passwordSalt,
+    },
+  });
+  return NextResponse.json(user, { status: 201 });
+}
