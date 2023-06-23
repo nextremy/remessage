38f37d7f9a164f9c0a5dd1b6e413728bb84bded8
Remy Wood <99709675+nextremy@users.noreply.github.com>
2023-06-23 13:20:35 -0700

    Enable user login

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,11 @@
-import { fastifyAutoload } from "@fastify/autoload";
-import { fastify } from "fastify";
+import fastifyAutoload from "@fastify/autoload";
+import fastifyCookie from "@fastify/cookie";
+import fastify from "fastify";
 import { join } from "path";
 
 const app = fastify({ logger: process.env.NODE_ENV === "development" });
 
+app.register(fastifyCookie);
 app.register(fastifyAutoload, { dir: join(__dirname, "routes") });
 
 app.listen({ port: 4000 }).catch((error) => {
