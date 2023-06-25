1fd15e417955b34b148e7aa01444701e048c560e
Remy Wood <99709675+nextremy@users.noreply.github.com>
2023-06-24 19:24:55 -0700

    Reorder user routes

--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -4,6 +4,36 @@ import { db } from "../database/client";
 import { AppInstance } from "../types/AppInstance";
 
 export default async function (app: AppInstance) {
+  app.post(
+    "/users",
+    {
+      schema: {
+        body: Type.Object({
+          username: Type.String({
+            minLength: 1,
+            maxLength: 16,
+            pattern: "^[a-z0-9_]*$",
+          }),
+          password: Type.String({
+            minLength: 8,
+            maxLength: 256,
+          }),
+        }),
+      },
+    },
+    async (request) => {
+      const { username } = request.body;
+
+      const passwordHash = await hash(request.body.password);
+      await db.user.create({
+        data: {
+          username,
+          passwordHash,
+        },
+      });
+    },
+  );
+
   app.get(
     "/users/:userId",
     {
@@ -33,36 +63,6 @@ export default async function (app: AppInstance) {
         id: user.id,
         username: user.username,
       };
-    },
-  );
-
-  app.post(
-    "/users",
-    {
-      schema: {
-        body: Type.Object({
-          username: Type.String({
-            minLength: 1,
-            maxLength: 16,
-            pattern: "^[a-z0-9_]*$",
-          }),
-          password: Type.String({
-            minLength: 8,
-            maxLength: 256,
-          }),
-        }),
-      },
-    },
-    async (request) => {
-      const { username } = request.body;
-
-      const passwordHash = await hash(request.body.password);
-      await db.user.create({
-        data: {
-          username,
-          passwordHash,
-        },
-      });
     },
   );
 }
