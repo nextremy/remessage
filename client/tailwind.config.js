57d4cf9eb2cc011a8046eb9f28c1b08f2905ee5d
Remy Wood <99709675+nextremy@users.noreply.github.com>
2023-06-30 10:52:59 -0700

    Add Work Sans

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,14 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import("tailwindcss").Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Work Sans", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
