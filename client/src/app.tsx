1541c5dfb696aa26c947c6046606557d1630009a
Remy Wood <99709675+nextremy@users.noreply.github.com>
2023-06-29 22:46:25 -0700

    Set up tRPC client

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,3 +1,30 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { httpBatchLink } from "@trpc/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { trpc } from "./trpc";
+
+const queryClient = new QueryClient();
+const trpcClient = trpc.createClient({
+  links: [
+    httpBatchLink({
+      url: "http://localhost:4000",
+      headers: () => ({
+        authorization: localStorage.getItem("token") ?? undefined,
+      }),
+    }),
+  ],
+});
+
 export default function App() {
-  return <>App</>;
+  return (
+    <trpc.Provider client={trpcClient} queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<div />} path="/" />
+          </Routes>
+        </BrowserRouter>
+      </QueryClientProvider>
+    </trpc.Provider>
+  );
 }
