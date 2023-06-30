81bcb5c31325640d4116da954b7fc5f92ad86b3d
Remy Wood <99709675+nextremy@users.noreply.github.com>
2023-06-30 13:51:06 -0700

    Implement friend list rendering

--- client/src/routes/friends.tsx
+++ client/src/routes/friends.tsx
@@ -1,3 +1,16 @@
+import { trpc } from "../trpc";
+
 export function FriendsRoute() {
-  return <div></div>;
+  const friendListQuery = trpc.friend.list.useQuery();
+
+  if (!friendListQuery.data) return null;
+  return (
+    <ul className="p-4">
+      {friendListQuery.data.map((friend) => (
+        <li className="font-medium" key={friend.id}>
+          {friend.username}
+        </li>
+      ))}
+    </ul>
+  );
 }
