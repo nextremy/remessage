1c036bc9c8608d47dfa8d235682586da04b22424
Remy Wood <99709675+nextremy@users.noreply.github.com>
2023-07-03 09:12:13 -0700

    Customize Tailwind colors

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,16 @@
+const colors = require("tailwindcss/colors");
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import("tailwindcss").Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      gray: colors.zinc,
+      blue: colors.blue,
+    },
     extend: {
       fontFamily: {
         sans: ["Work Sans", ...defaultTheme.fontFamily.sans],
