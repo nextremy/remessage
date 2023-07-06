b5dca0ca39d38b4bcf136b218768df5c51eda592
Remy Wood <99709675+nextremy@users.noreply.github.com>
2023-07-06 16:33:07 -0700

    Move click handler to button

--- client/src/components/AppBar.tsx
+++ client/src/components/AppBar.tsx
@@ -6,8 +6,11 @@ export function AppBar(props: { title: string }) {
 
   return (
     <div className="flex h-12 items-center border-b border-gray-300">
-      <button className="grid h-12 w-12 place-items-center">
-        <ArrowLeftIcon className="h-5 w-5" onClick={() => navigate(-1)} />
+      <button
+        className="grid h-12 w-12 place-items-center"
+        onClick={() => navigate(-1)}
+      >
+        <ArrowLeftIcon className="h-5 w-5" />
       </button>
       <h1 className="font-semibold">{props.title}</h1>
     </div>
