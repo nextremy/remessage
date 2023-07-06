a6139f2663936fead425472fe7f33d0be163281d
Remy Wood <99709675+nextremy@users.noreply.github.com>
2023-07-06 15:16:05 -0700

    Remove redunant elements

--- client/src/routes/login.tsx
+++ client/src/routes/login.tsx
@@ -21,41 +21,42 @@ export function LoginRoute() {
 
   return (
     <div className="grid min-h-screen place-items-center p-4">
-      <div className="w-full max-w-sm rounded border border-gray-300 p-6">
+      <form
+        className="flex w-full max-w-sm flex-col rounded border border-gray-300 p-6"
+        onSubmit={handleSubmit}
+      >
         <h1 className="text-center text-2xl font-bold">Log in</h1>
-        <form className="flex flex-col" onSubmit={handleSubmit}>
-          <label
-            className="mt-6 text-sm font-semibold tracking-wide text-gray-700"
-            htmlFor="username"
-          >
-            Username
-          </label>
-          <input
-            className="mt-2 h-12 rounded bg-gray-300 px-4"
-            name="username"
-            onChange={(event) => setUsername(event.target.value)}
-            type="text"
-          />
-          <label
-            className="mt-6 text-sm font-semibold tracking-wide text-gray-700"
-            htmlFor="password"
-          >
-            Password
-          </label>
-          <input
-            className="mt-2 h-12 rounded bg-gray-300 px-4"
-            name="password"
-            onChange={(event) => setPassword(event.target.value)}
-            type="password"
-          />
-          <button
-            className="mt-8 h-14 rounded bg-blue-700 text-lg font-bold text-gray-50"
-            type="submit"
-          >
-            Log in
-          </button>
-        </form>
-      </div>
+        <label
+          className="mt-6 text-sm font-semibold tracking-wide text-gray-700"
+          htmlFor="username"
+        >
+          Username
+        </label>
+        <input
+          className="mt-2 h-12 rounded bg-gray-300 px-4"
+          name="username"
+          onChange={(event) => setUsername(event.target.value)}
+          type="text"
+        />
+        <label
+          className="mt-6 text-sm font-semibold tracking-wide text-gray-700"
+          htmlFor="password"
+        >
+          Password
+        </label>
+        <input
+          className="mt-2 h-12 rounded bg-gray-300 px-4"
+          name="password"
+          onChange={(event) => setPassword(event.target.value)}
+          type="password"
+        />
+        <button
+          className="mt-8 h-14 rounded bg-blue-700 text-lg font-bold text-gray-50"
+          type="submit"
+        >
+          Log in
+        </button>
+      </form>
     </div>
   );
 }
