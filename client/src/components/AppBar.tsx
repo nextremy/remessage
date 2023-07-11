3378dfab4092579ba5966f65d131b5ab506b1986
Remy Wood <99709675+nextremy@users.noreply.github.com>
2023-07-11 11:57:35 -0700

    Remove back button from app bar

--- client/src/components/AppBar.tsx
+++ client/src/components/AppBar.tsx
@@ -1,17 +1,6 @@
-import { ArrowLeftIcon } from "@heroicons/react/20/solid";
-import { useNavigate } from "react-router-dom";
-
 export function AppBar(props: { title: string }) {
-  const navigate = useNavigate();
-
   return (
-    <div className="flex h-12 items-center border-b border-gray-300">
-      <button
-        className="grid h-12 w-12 place-items-center"
-        onClick={() => navigate(-1)}
-      >
-        <ArrowLeftIcon className="h-5 w-5" />
-      </button>
+    <div className="flex h-12 items-center border-b border-gray-300 px-4">
       <h1 className="font-semibold">{props.title}</h1>
     </div>
   );
