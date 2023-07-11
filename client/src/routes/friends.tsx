dfefab435738f0f8d0011c9e395d06eb0cf17c74
Remy Wood <99709675+nextremy@users.noreply.github.com>
2023-07-11 12:48:31 -0700

    Redesign friends route

--- client/src/routes/friends.tsx
+++ client/src/routes/friends.tsx
@@ -1,102 +1,34 @@
-import { Dialog } from "@headlessui/react";
-import {
-  ArrowsRightLeftIcon,
-  ChatBubbleLeftIcon,
-  UserPlusIcon,
-  XMarkIcon,
-} from "@heroicons/react/20/solid";
-import { FormEvent, useState } from "react";
+import { Tab } from "@headlessui/react";
+import { ChatBubbleLeftIcon, XMarkIcon } from "@heroicons/react/20/solid";
 import { Link } from "react-router-dom";
 import { AppBar } from "../components/AppBar";
 import { trpc } from "../trpc";
 
 export function FriendsRoute() {
   return (
-    <>
+    <Tab.Group>
       <AppBar>
         <AppBar.Title>Friends</AppBar.Title>
+        <Tab.List className="ml-4 flex h-8 gap-2 border-l border-gray-300 pl-4">
+          <Tab className="ui-selected:bg-gray-200 ui-selected:text-gray-900 rounded px-4 font-medium text-gray-600 hover:bg-gray-200 hover:text-gray-900">
+            List
+          </Tab>
+          <Tab className="ui-selected:bg-gray-200 ui-selected:text-gray-900 rounded px-4 font-medium text-gray-600 hover:bg-gray-200 hover:text-gray-900">
+            Pending
+          </Tab>
+          <Tab className="ui-selected:bg-gray-200 ui-selected:text-gray-900 rounded px-4 font-medium text-gray-600 hover:bg-gray-200 hover:text-gray-900">
+            Add friend
+          </Tab>
+        </Tab.List>
       </AppBar>
-      <div className="flex flex-col gap-2 py-2">
-        <AddFriendButton />
-        <FriendRequestsButton />
-        <FriendsList />
-      </div>
-    </>
-  );
-}
-
-function AddFriendButton() {
-  const [dialogOpen, setDialogOpen] = useState(false);
-  const [friendUsername, setFriendUsername] = useState("");
-  const friendRequestCreateMutation = trpc.friendRequest.create.useMutation({
-    onSuccess: () => setDialogOpen(false),
-  });
-
-  function sendFriendRequest(event: FormEvent<HTMLFormElement>) {
-    event.preventDefault();
-    friendRequestCreateMutation.mutate({ receiverUsername: friendUsername });
-  }
-
-  return (
-    <>
-      <button
-        className="mx-2 flex h-12 items-center gap-2 rounded px-4 font-semibold text-gray-700 duration-200 hover:bg-gray-200"
-        onClick={() => setDialogOpen(true)}
-      >
-        <UserPlusIcon className="h-5 w-5" />
-        Add friend
-      </button>
-      <Dialog
-        className="relative z-50"
-        onClose={() => setDialogOpen(false)}
-        open={dialogOpen}
-      >
-        <div className="fixed inset-0 bg-gray-950/50" />
-        <div className="fixed inset-0 grid place-items-center p-4">
-          <Dialog.Panel className="w-full max-w-sm rounded border border-gray-300 bg-gray-50 p-4">
-            <Dialog.Title className="text-xl font-medium">
-              Add friend
-            </Dialog.Title>
-            <Dialog.Description>
-              Add a friend by their username.
-            </Dialog.Description>
-            <form className="flex flex-col" onSubmit={sendFriendRequest}>
-              <label
-                className="mt-4 text-sm font-semibold tracking-wide text-gray-700"
-                htmlFor="Friend's username"
-              >
-                Friend{"'"}s username
-              </label>
-              <input
-                className="mt-2 h-12 rounded bg-gray-300 px-4"
-                name="Friend's username"
-                onChange={(event) => setFriendUsername(event.target.value)}
-                type="text"
-              />
-              <button
-                className="mt-2 h-12 rounded bg-blue-700 font-bold text-gray-50 duration-200 hover:bg-blue-600 disabled:opacity-50"
-                disabled={friendUsername === ""}
-                type="submit"
-              >
-                Send friend request
-              </button>
-            </form>
-          </Dialog.Panel>
-        </div>
-      </Dialog>
-    </>
-  );
-}
-
-function FriendRequestsButton() {
-  return (
-    <Link
-      className="mx-2 flex h-12 items-center gap-2 rounded px-4 font-semibold text-gray-700 duration-200 hover:bg-gray-200"
-      to="/friends/requests"
-    >
-      <ArrowsRightLeftIcon className="h-5 w-5" />
-      Friend requests
-    </Link>
+      <Tab.Panels className="flex flex-col gap-2 py-2">
+        <Tab.Panel>
+          <FriendsList />
+        </Tab.Panel>
+        <Tab.Panel></Tab.Panel>
+        <Tab.Panel></Tab.Panel>
+      </Tab.Panels>
+    </Tab.Group>
   );
 }
 
@@ -113,12 +45,12 @@ function FriendsList() {
           {friend.username}
           <div className="flex gap-2">
             <Link
-              className="grid h-12 w-12 place-items-center rounded-full bg-gray-300 text-gray-700 duration-200 hover:bg-gray-400"
+              className="grid h-12 w-12 place-items-center rounded-full bg-gray-200 text-gray-700 hover:bg-gray-300 hover:text-gray-900"
               to={`/direct-chats/${friend.id}`}
             >
               <ChatBubbleLeftIcon className="h-5 w-5" />
             </Link>
-            <button className="grid h-12 w-12 place-items-center rounded-full bg-gray-300 text-gray-700 duration-200 hover:bg-gray-400">
+            <button className="grid h-12 w-12 place-items-center rounded-full bg-gray-200 text-gray-700 hover:bg-gray-300 hover:text-gray-900">
               <XMarkIcon className="h-6 w-6" />
             </button>
           </div>
