b5819585ffb9b77c6872b7605964980b146998e2
Remy Wood <99709675+nextremy@users.noreply.github.com>
2023-07-11 12:15:06 -0700

    Remove redunant h1

--- client/src/components/AppBar.tsx
+++ client/src/components/AppBar.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from "react";
 export function AppBar(props: { children: ReactNode }) {
   return (
     <div className="flex h-12 items-center border-b border-gray-300 px-4">
-      <h1 className="font-semibold">{props.children}</h1>
+      {props.children}
     </div>
   );
 }
