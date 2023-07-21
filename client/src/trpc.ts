f77842b115ce97eb9419fa36ba2f1bcbb1442db8
Remy Wood <99709675+nextremy@users.noreply.github.com>
2023-07-21 15:33:09 -0700

    Set up tRPC client

---
+++ client/src/trpc.ts
@@ -0,0 +1,17 @@
+import { createTRPCProxyClient, httpBatchLink } from "@trpc/client";
+import type { AppRouter } from "../../server/src/index";
+
+const trpc = createTRPCProxyClient<AppRouter>({
+  links: [
+    httpBatchLink({
+      url: "http://localhost:4000",
+      headers: () => {
+        const token = localStorage.getItem("token");
+        if (token === null) return {};
+        return { Authorization: `Bearer ${token}` };
+      },
+    }),
+  ],
+});
+
+export default trpc;
