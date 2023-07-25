85b23243dcf1918cf4b6b8cb19d92f46eba0d015
Remy Wood <99709675+nextremy@users.noreply.github.com>
2023-07-25 15:23:28 -0700

    Redirect unauthenticated users to login

--- client/src/layouts/main/index.tsx
+++ client/src/layouts/main/index.tsx
@@ -1,7 +1,14 @@
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
+import { useSession } from "../../hooks/session";
 import { FriendsLink } from "./friends-link";
 
 export function MainLayout() {
+  try {
+    useSession();
+  } catch (error) {
+    return <Navigate to="/login" />;
+  }
+
   return (
     <div className="flex h-screen">
       <div className="w-80 bg-gray-200 p-4">
