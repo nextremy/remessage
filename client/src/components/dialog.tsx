c5113e7022a2f87e89b2db65032bb4868af0dfaa
Remy Wood <99709675+nextremy@users.noreply.github.com>
2023-07-25 11:45:25 -0700

    Create dialog component

---
+++ client/src/components/dialog.tsx
@@ -0,0 +1,27 @@
+import { Dialog as HUIDialog } from "@headlessui/react";
+import { ReactNode } from "react";
+
+export function Dialog(props: {
+  open: boolean;
+  onClose: () => void;
+  title: ReactNode;
+  description: ReactNode;
+  children: ReactNode;
+}) {
+  return (
+    <HUIDialog
+      className="fixed inset-0 grid place-items-center"
+      onClose={props.onClose}
+      open={props.open}
+    >
+      <div className="fixed inset-0 bg-gray-900/50" />
+      <HUIDialog.Panel className="z-50 w-full max-w-sm rounded-md border-gray-200 bg-gray-100 p-4">
+        <HUIDialog.Title className="text-xl font-bold">
+          {props.title}
+        </HUIDialog.Title>
+        <HUIDialog.Description>{props.description}</HUIDialog.Description>
+        {props.children}
+      </HUIDialog.Panel>
+    </HUIDialog>
+  );
+}
