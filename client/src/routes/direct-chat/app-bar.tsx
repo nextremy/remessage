edcad90adfcc1e7acbf79736747faa9108fcee90
Remy Wood <99709675+nextremy@users.noreply.github.com>
2023-07-25 15:08:40 -0700

    Add app bar to direct chat route

---
+++ client/src/routes/direct-chat/app-bar.tsx
@@ -0,0 +1,14 @@
+import { useParams } from "react-router-dom";
+import { trpc } from "../../trpc";
+
+export function AppBar() {
+  const { directChatId } = useParams();
+  const { data: user } = trpc.user.get.useQuery({ userId: directChatId! });
+
+  if (!user) return null;
+  return (
+    <div className="flex h-12 items-center border-b-2 border-gray-200 px-4">
+      <h1 className="font-medium">@{user.username}</h1>
+    </div>
+  );
+}
--- client/src/routes/direct-chat/index.tsx
+++ client/src/routes/direct-chat/index.tsx
@@ -1,11 +1,15 @@
+import { AppBar } from "./app-bar";
 import { MessageInput } from "./message-input";
 import { MessageList } from "./message-list";
 
 export function DirectChatRoute() {
   return (
-    <div className="flex grow flex-col gap-4 p-4">
-      <MessageList />
-      <MessageInput />
+    <div className="flex grow flex-col">
+      <AppBar />
+      <div className="flex grow flex-col gap-4 p-4">
+        <MessageList />
+        <MessageInput />
+      </div>
     </div>
   );
 }
