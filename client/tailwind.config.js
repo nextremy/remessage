0bd07ac1f8fd8c67853333352f30b3554c57043c
Remy Wood <99709675+nextremy@users.noreply.github.com>
2023-07-24 21:20:02 -0700

    Scaffold React client with Vite

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,12 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
 /** @type {import("tailwindcss").Config} */
-module.exports = {
-  content: [
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      sans: ["Inter", ...defaultTheme.fontFamily.sans],
+    },
     extend: {},
   },
   plugins: [],
