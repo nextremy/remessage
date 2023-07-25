3d3e4b398d67a72a7b0ef1609ad022d999608bc6
Remy Wood <99709675+nextremy@users.noreply.github.com>
2023-07-25 15:20:58 -0700

    Refactor friends link to dedicated component

--- client/src/layouts/main/index.tsx
+++ client/src/layouts/main/index.tsx
@@ -1,22 +1,11 @@
-import { UsersIcon } from "@heroicons/react/20/solid";
-import { Link, Outlet, useLocation } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import { FriendsLink } from "./friends-link";
 
 export function MainLayout() {
-  const { pathname } = useLocation();
-
   return (
     <div className="flex h-screen">
       <div className="w-80 bg-gray-200 p-4">
-        <Link
-          className={`${
-            pathname.startsWith("/friends")
-              ? "bg-gray-300 text-gray-900"
-              : "text-gray-700"
-          } flex h-12 items-center gap-2 rounded-md px-4 font-medium hover:bg-gray-300 hover:text-gray-900`}
-          to="friends"
-        >
-          <UsersIcon className="h-5 w-5" /> Friends
-        </Link>
+        <FriendsLink />
       </div>
       <Outlet />
     </div>
