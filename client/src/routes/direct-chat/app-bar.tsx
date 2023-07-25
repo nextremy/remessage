502623640543f0b355bff9608c68026c823dbcbc
Remy Wood <99709675+nextremy@users.noreply.github.com>
2023-07-25 15:30:09 -0700

    Bolden app bar text

--- client/src/routes/direct-chat/app-bar.tsx
+++ client/src/routes/direct-chat/app-bar.tsx
@@ -8,7 +8,7 @@ export function AppBar() {
   if (!user) return null;
   return (
     <div className="flex h-12 items-center border-b-2 border-gray-200 px-4">
-      <h1 className="font-medium">@{user.username}</h1>
+      <h1 className="font-semibold">@{user.username}</h1>
     </div>
   );
 }
