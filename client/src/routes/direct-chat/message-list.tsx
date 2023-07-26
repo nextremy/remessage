ee1e920bdc76b54c3fb7c8cfe3e58762af7cbecd
Remy Wood <99709675+nextremy@users.noreply.github.com>
2023-07-26 11:43:23 -0700

    Implement realtime message streaming

--- client/src/routes/direct-chat/message-list.tsx
+++ client/src/routes/direct-chat/message-list.tsx
@@ -9,13 +9,38 @@ export function MessageList() {
     userIds: [userId, otherUserId!],
     limit: 50,
   });
+  const context = trpc.useContext();
+  trpc.directMessage.stream.useSubscription(
+    { userIds: [userId, otherUserId!] },
+    {
+      onData: (newMessage) => {
+        context.directMessage.list.setData(
+          { userIds: [userId, otherUserId!], limit: 50 },
+          (oldMessages) => {
+            if (!oldMessages) return [newMessage];
+            if (
+              oldMessages.some((oldMessage) => oldMessage.id === newMessage.id)
+            ) {
+              return oldMessages;
+            }
+            return [...oldMessages, newMessage];
+          },
+        );
+      },
+    },
+  );
   const { data: otherUser } = trpc.user.get.useQuery({ userId: otherUserId! });
 
   if (!messages) return null;
   if (!otherUser) return null;
+  const sortedMessages = [...messages].sort((message1, message2) => {
+    if (message1.timestamp < message2.timestamp) return -1;
+    if (message1.timestamp > message2.timestamp) return 1;
+    return 0;
+  });
   return (
     <ul className="flex grow flex-col justify-end">
-      {messages.map((message) => (
+      {sortedMessages.map((message) => (
         <li key={message.id}>
           <div className="flex items-center gap-2">
             <p className="font-medium">{message.sender.username}</p>
