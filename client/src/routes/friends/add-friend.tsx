80c1be4af9f8a2748dd1e2af7c9baa970e3fac79
Remy Wood <99709675+nextremy@users.noreply.github.com>
2023-07-26 09:09:09 -0700

    Specify input ID variable name

--- client/src/routes/friends/add-friend.tsx
+++ client/src/routes/friends/add-friend.tsx
@@ -7,7 +7,7 @@ export function AddFriend() {
   const { mutate: createFriendRequest } = trpc.friendRequest.create.useMutation(
     { onSuccess: () => setFriendUsername("") },
   );
-  const inputId = useId();
+  const friendUsernameInputId = useId();
   const [friendUsername, setFriendUsername] = useState("");
 
   return (
@@ -25,14 +25,14 @@ export function AddFriend() {
       <p>You can add another user as a friend by their username.</p>
       <label
         className="mt-4 block text-sm font-medium tracking-wide"
-        htmlFor={inputId}
+        htmlFor={friendUsernameInputId}
       >
         Friend{"'"}s username
       </label>
       <div className="mt-1 flex gap-2">
         <input
           className="h-12 grow rounded-md bg-gray-300 px-4"
-          id={inputId}
+          id={friendUsernameInputId}
           onChange={(event) => setFriendUsername(event.target.value)}
           type="text"
           value={friendUsername}
