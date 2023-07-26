c6ae9e04f89d797537c94cbec12230e2fee53467
Remy Wood <99709675+nextremy@users.noreply.github.com>
2023-07-26 13:31:01 -0700

    Adjust settings link styling

--- client/src/layouts/main/settings-link.tsx
+++ client/src/layouts/main/settings-link.tsx
@@ -7,8 +7,10 @@ export function SettingsLink() {
   return (
     <Link
       className={`${
-        pathname.startsWith("/settings") ? "brightness-90" : ""
-      } grid h-10 w-10 place-items-center rounded-full bg-gray-300 hover:brightness-90`}
+        pathname.startsWith("/settings")
+          ? "text-gray-900 brightness-90"
+          : "text-gray-700"
+      } grid h-10 w-10 place-items-center rounded-full bg-gray-300 hover:text-gray-900 hover:brightness-90`}
       to="settings"
     >
       <Cog6ToothIcon className="h-5 w-5" />
