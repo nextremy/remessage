6615bd95f7324480a901d423d6c0d6fdd68f2600
Remy Wood <99709675+nextremy@users.noreply.github.com>
2023-07-30 12:30:01 -0700

    Configure Tailwind colors

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,10 +1,35 @@
 import headlessUI from "@headlessui/tailwindcss";
+import colors from "tailwindcss/colors";
 import defaultTheme from "tailwindcss/defaultTheme";
 
 /** @type {import("tailwindcss").Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      current: "currentColor",
+      transparent: "transparent",
+      black: colors.black,
+      white: colors.white,
+      gray: colors.slate,
+      red: colors.red,
+      orange: colors.orange,
+      amber: colors.amber,
+      yellow: colors.amber,
+      lime: colors.lime,
+      green: colors.green,
+      emerald: colors.emerald,
+      teal: colors.teal,
+      cyan: colors.cyan,
+      sky: colors.sky,
+      blue: colors.blue,
+      indigo: colors.indigo,
+      violet: colors.violet,
+      purple: colors.purple,
+      fuchsia: colors.fuchsia,
+      pink: colors.pink,
+      rose: colors.rose,
+    },
     fontFamily: {
       sans: ["Inter", ...defaultTheme.fontFamily.sans],
     },
