b89efd50c69ababe234829b10114af5aa95b52b2
Remy Wood <99709675+nextremy@users.noreply.github.com>
2023-08-02 09:31:20 -0700

    Set up tRPC client

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,6 +1,7 @@
-import type { Metadata } from "next";
+import { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Providers } from "./providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -8,16 +9,14 @@ export const metadata: Metadata = {
   title: "Remessage",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout(props: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body className={`bg-gray-100 text-gray-900 ${inter.className}`}>
-        {children}
-      </body>
-    </html>
+    <Providers>
+      <html lang="en">
+        <body className={`bg-gray-100 text-gray-900 ${inter.className}`}>
+          {props.children}
+        </body>
+      </html>
+    </Providers>
   );
 }
