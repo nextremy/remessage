0c73c5d126f834af56afad03afda5c96117d5771
Remy Wood <99709675+nextremy@users.noreply.github.com>
2023-08-02 13:08:01 -0700

    Implement friend requests button

--- client/src/app/(app)/layout.tsx
+++ client/src/app/(app)/layout.tsx
@@ -4,7 +4,13 @@ import { Dialog } from "@/components/dialog";
 import { useSession } from "@/hooks/use-session";
 import { trpc } from "@/trpc";
 import { Tab } from "@headlessui/react";
-import { ChatBubbleLeftIcon, UserPlusIcon } from "@heroicons/react/20/solid";
+import {
+  ArrowsRightLeftIcon,
+  ChatBubbleLeftIcon,
+  CheckIcon,
+  UserPlusIcon,
+  XMarkIcon,
+} from "@heroicons/react/20/solid";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { ReactNode, useId, useState } from "react";
@@ -32,8 +38,9 @@ export default function AppLayout(props: { children: ReactNode }) {
             <Tab.Panel>
               <ChatsList />
             </Tab.Panel>
-            <Tab.Panel className="flex flex-col p-4">
+            <Tab.Panel className="flex flex-col gap-2 p-4">
               <AddFriendButton />
+              <FriendRequestsButton />
               <FriendsList />
             </Tab.Panel>
           </Tab.Panels>
@@ -129,6 +136,117 @@ function AddFriendButton() {
         </form>
       </Dialog>
     </>
+  );
+}
+
+function FriendRequestsButton() {
+  const [dialogOpen, setDialogOpen] = useState(false);
+
+  return (
+    <>
+      <button
+        className="flex h-14 w-full items-center gap-2 rounded-md bg-gray-200 px-4 font-semibold text-gray-700 duration-200 hover:brightness-95"
+        onClick={() => setDialogOpen(true)}
+      >
+        <ArrowsRightLeftIcon className="h-5 w-5" /> Friend requests
+      </button>
+      <Dialog open={dialogOpen} onClose={() => setDialogOpen(false)}>
+        <div className="flex flex-col">
+          <Dialog.Title>Friend requests</Dialog.Title>
+          <FriendRequestsList />
+          <div className="h-4" />
+          <button
+            className="h-12 rounded-md bg-gray-200 px-4 font-semibold duration-200 hover:brightness-95"
+            onClick={() => setDialogOpen(false)}
+          >
+            Close
+          </button>
+        </div>
+      </Dialog>
+    </>
+  );
+}
+
+function FriendRequestsList() {
+  const session = useSession();
+  const { data: friendRequests } = trpc.friendRequest.list.useQuery(
+    { userId: session?.userId || "" },
+    { enabled: session !== null },
+  );
+
+  if (friendRequests?.length === 0) {
+    return (
+      <p className="mt-2 grid h-16 place-items-center font-light">
+        No friend requests to show
+      </p>
+    );
+  }
+  return (
+    <ul>
+      {friendRequests
+        ? friendRequests.map((friendRequest) => (
+            <FriendRequestsListItem
+              friendRequest={friendRequest}
+              key={friendRequest.id}
+            />
+          ))
+        : null}
+    </ul>
+  );
+}
+
+function FriendRequestsListItem(props: {
+  friendRequest: {
+    id: string;
+    sender: { username: string; id: string };
+    receiver: { username: string; id: string };
+  };
+}) {
+  const session = useSession();
+  const context = trpc.useContext();
+  const { mutate: acceptFriendRequest } = trpc.friendRequest.accept.useMutation(
+    {
+      onSuccess: () => {
+        context.friend.list.invalidate();
+        context.friendRequest.list.invalidate();
+      },
+    },
+  );
+  const { mutate: removeFriendRequest } = trpc.friendRequest.delete.useMutation(
+    {
+      onSuccess: () => context.friendRequest.list.invalidate(),
+    },
+  );
+
+  const isOutgoing = session?.userId === props.friendRequest.sender.id;
+  return (
+    <li className="flex h-16 items-center justify-between">
+      <div>
+        <p className="font-medium">
+          {isOutgoing
+            ? props.friendRequest.receiver.username
+            : props.friendRequest.sender.username}
+        </p>
+        <p className="text-sm font-light tracking-wide">
+          {isOutgoing ? "Outgoing friend request" : "Incoming friend request"}
+        </p>
+      </div>
+      <div className="flex gap-2">
+        <button
+          className="grid h-12 w-12 place-items-center rounded-full bg-gray-200 text-gray-600 duration-200 hover:text-gray-900 hover:brightness-95"
+          onClick={() => acceptFriendRequest({ id: props.friendRequest.id })}
+        >
+          <CheckIcon className="h-5 w-5" />
+        </button>
+        <button
+          className="grid h-12 w-12 place-items-center rounded-full bg-gray-200 text-gray-600 duration-200 hover:text-gray-900 hover:brightness-95"
+          onClick={() => removeFriendRequest({ id: props.friendRequest.id })}
+          title="Remove"
+        >
+          <XMarkIcon className="h-5 w-5" />
+        </button>
+      </div>
+    </li>
   );
 }
 
