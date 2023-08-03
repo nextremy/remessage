0145c759c8b778eee97d269a3856fb6019c731e7
Remy Wood <99709675+nextremy@users.noreply.github.com>
2023-08-02 18:49:54 -0700

    Add chevron to display on current chat

--- client/src/layouts/app/chats-list.tsx
+++ client/src/layouts/app/chats-list.tsx
@@ -1,3 +1,4 @@
+import { ChevronRightIcon } from "@heroicons/react/24/solid";
 import { Link, useLocation } from "react-router-dom";
 import { useSession } from "../../hooks/use-session";
 import { trpc } from "../../trpc";
@@ -28,17 +29,19 @@ function ChatsListItem(props: {
   const { pathname } = useLocation();
 
   const user = props.chat.users.filter((user) => user.id !== session.userId)[0];
+  const isCurrentChat = pathname.startsWith(`/chats/${props.chat.id}`);
   return (
     <li>
       <Link
-        className={`flex h-16 items-center px-4 font-medium duration-150 hover:bg-gray-200 hover:text-gray-900 ${
-          pathname.startsWith(`/chats/${props.chat.id}`)
-            ? "bg-gray-200 text-gray-900"
-            : "text-gray-700"
+        className={`flex h-16 items-center justify-between px-4 font-medium duration-150 hover:bg-gray-200 hover:text-gray-900 ${
+          isCurrentChat ? "bg-gray-200 text-gray-900" : "text-gray-700"
         }`}
         to={`/chats/${props.chat.id}`}
       >
         {user.username}
+        <ChevronRightIcon
+          className={`h-6 w-6 ${isCurrentChat ? "" : "hidden"}`}
+        />
       </Link>
     </li>
   );
