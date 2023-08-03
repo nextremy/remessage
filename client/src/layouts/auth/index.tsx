96fb7f1f66be26125426a9d8d375654906246408
Remy Wood <99709675+nextremy@users.noreply.github.com>
2023-08-03 10:20:39 -0700

    Simplify automatic navigation from auth routes

--- client/src/layouts/auth/index.tsx
+++ client/src/layouts/auth/index.tsx
@@ -1,13 +1,10 @@
-import { useEffect } from "react";
-import { Outlet, useNavigate } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
+import { useSession } from "../../hooks/use-session";
 
 export function AuthLayout() {
-  const navigate = useNavigate();
-  useEffect(() => {
-    const token = localStorage.getItem("token");
-    if (token !== null) navigate("/", { replace: true });
-  }, [navigate]);
+  const session = useSession();
 
+  if (session) return <Navigate replace to="/login" />;
   return (
     <div className="grid h-screen place-items-center bg-gray-100 text-gray-900">
       <div className="w-full max-w-sm rounded-md border-2 border-gray-200 p-4">
